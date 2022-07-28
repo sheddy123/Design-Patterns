cb45fdd455cbf102ad6c6ee534f740267aa411a7
IfeanyiOdom <ifeanyiodom@mtu.edu.ng>
2022-07-28 04:59:19 +0100

    Commit staged

---
+++ Creational_Patterns/Factory Pattern/FactoryMethod/Employee/BaseEmployeeFactory.cs
@@ -0,0 +1,22 @@
+﻿using Creational_Patterns.Managers;
+using Creational_Patterns.Models;
+
+namespace Creational_Patterns.Factory_Pattern.FactoryMethod
+{
+    public abstract class BaseEmployeeFactory
+    {
+        protected Employee _emp;
+        public BaseEmployeeFactory(Employee emp)
+        {
+            this._emp = emp;
+        }
+        public Employee ApplySalary()
+        {
+            IEmployeeManager manager = this.Create();
+            _emp.Bonus = manager.GetBonus();
+            _emp.HourlyPay = manager.GetPay();
+            return _emp;
+        }
+        public abstract IEmployeeManager Create();
+    }
+}
