ba38189a8b44e91cd0cf1b1e400b781f1bab06c8
IfeanyiOdom <ifeanyiodom@mtu.edu.ng>
2022-07-28 04:58:44 +0100

    Add simple factory implementation example

---
+++ Creational_Patterns/SimpleFactory/Pizza.cs
@@ -0,0 +1,11 @@
+﻿namespace Creational_Patterns.SimpleFactory
+{
+    public abstract class Pizza
+    {
+        public abstract void prepare();
+        public abstract void bake();
+        public abstract void cut();
+        public abstract void box();
+    }
+
+}
---
+++ Creational_Patterns/SimpleFactory/VeggiePizza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Creational_Patterns.SimpleFactory
+{
+    public class VeggiePizza : Pizza
+    {
+        public override void bake()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void box()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void cut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void prepare()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
